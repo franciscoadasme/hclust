9fb8eb395be2819cdfaf29a12d0ca96f991215cf
Francisco Adasme <francisco.adasme@gmail.com>
2022-03-01 12:39:00 -0300

    Split linkage methods into separate files

--- src/hclust/chain.cr
+++ src/hclust/chain.cr
@@ -1,29 +1,5 @@
 module HClust
   # TODO: docs
-  def self.mst(dism : DistanceMatrix) : Dendrogram
-    # keeps updated distances to merged nodes
-    dis_ptr = Pointer(Float64).malloc dism.size
-    # position 0 is never accessed because the search starts at node 1
-    (dis_ptr + 1).copy_from dism.to_unsafe, dism.size - 1
-
-    dendrogram = Dendrogram.new(dism.size - 1)
-    active_nodes = IndexList.new(dism.size)
-    n_i = 0 # current node
-    (dism.size - 1).times do
-      active_nodes.delete n_i
-      n_j, d_ij = active_nodes.nearest_to(n_i, dism) do |n_k, dis|
-        ptr = dis_ptr + n_k
-        Method.single(dis, ptr)
-        ptr.value
-      end
-      dendrogram << Dendrogram::Step.new(n_i, n_j, d_ij)
-      n_i = n_j
-    end
-
-    dendrogram
-  end
-
-  # TODO: docs
   def self.nn_chain(dism : DistanceMatrix,
                     method : ChainMethod,
                     reuse : Bool = false) : Dendrogram
